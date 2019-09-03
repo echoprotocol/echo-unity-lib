e4e8450bdb61f1d3dc68dde68d3f47a3c6027c0a
Max <max.michno@gmail.com>
2019-09-03 11:58:16 +0300

    Implement keccak256.

--- Scripts/BaseApi/Tools/Hash.cs
+++ Scripts/BaseApi/Tools/Hash.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Security.Cryptography;
+using DZen.Security.Cryptography;
 
 
 namespace Tools.Hash
 {
     public static class Extensions
     {
+        public static byte[] HashAndDispose(this SHA3 sha3, byte[] buffer)
+        {
+            var hash = sha3.ComputeHash(buffer);
+            (sha3 as IDisposable).Dispose();
+            return hash;
+        }
+
         public static byte[] HashAndDispose(this SHA256 sha256, byte[] buffer)
         {
             var hash = sha256.ComputeHash(buffer);
@@ -33,5 +41,11 @@ namespace Tools.Hash
             (hmacsha256 as IDisposable).Dispose();
             return hash;
         }
+    }
+
+
+    public static class KECCAK256
+    {
+        public static SHA3 Create() => new SHA3256Managed { UseKeccakPadding = true };
     }
 }
